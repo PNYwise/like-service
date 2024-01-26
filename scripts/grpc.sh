43549a6054cf2046267e5e1a70fa628d7244f8b2
pnywise <bunayya12345@gmail.com>
2024-01-26 21:48:23 +0700

    compile proto

---
+++ scripts/grpc.sh
@@ -0,0 +1,8 @@
+protoc --proto_path=social-media-proto \
+    --go_out=proto \
+    --go_opt=paths=source_relative \
+    --go-grpc_out=proto \
+    --go-grpc_opt=paths=source_relative \
+    social-media-proto/base.proto \
+    social-media-proto/like.proto \
+    social-media-proto/config.proto

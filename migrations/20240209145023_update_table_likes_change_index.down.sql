c83fa9d5a3ef33bd37a7ba81779d8269a03d8099
pnywise <bunayya12345@gmail.com>
2024-02-09 23:36:33 +0700

    fix migration

--- migrations/20240209145023_update_table_likes_change_index.down.sql
+++ migrations/20240209145023_update_table_likes_change_index.down.sql
@@ -1,3 +1,3 @@
-DROP INDEX idx_unique_user_post_uuid;
-
+DROP INDEX IF EXISTS idx_unique_user_post_uuid;
+ALTER TABLE public.likes DROP CONSTRAINT unique_user_and_post_uuid;
 CREATE INDEX IF NOT EXISTS idx_user_post_uuid ON likes (user_uuid,post_uuid);
--- migrations/20240209145023_update_table_likes_change_index.up.sql
+++ migrations/20240209145023_update_table_likes_change_index.up.sql
@@ -1,3 +1,3 @@
-DROP INDEX idx_user_post_uuid;
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_user_post_uuid ON likes (user_uuid,post_uuid);
+DROP INDEX IF EXISTS idx_user_post_uuid;
+ALTER TABLE public.likes ADD CONSTRAINT unique_user_and_post_uuid UNIQUE (user_uuid,post_uuid);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_user_post_uuid ON public.likes (user_uuid,post_uuid);
